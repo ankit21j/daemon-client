bd6517b90319f0658c48ff4921f5f2eb65b2c584
Ankit Gupta <ankit@original4sure.com>
2018-11-29 14:13:47 +0530

    handle mongo closing event

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,6 @@ const connectionUri: string = "mongodb://127.0.0.1:27017/test-client-db"
 // const db = "test-client-db"
 
 const main = async () => {
-
   // establish a webSocket connection
   wss.on("connection", (ws: WebSocket) => {
     ws.on("message", (message: string) => {
@@ -39,19 +38,27 @@ const main = async () => {
 
 const connectToMongo = async (uriConnect: string, retryNumber: number) => {
   try {
-    await dbManager.connect(uriConnect)
+    const mDb = await dbManager.connect(uriConnect)
     logger.info("Successfully connected to mongodb")
+    mDb.on("close", () => {
+      logger.error("Error! Mongo closed")
+      retryMongoConnection(uriConnect, retryNumber)
+    })
     return
   } catch (error) {
     logger.error("\n", error)
 
     // retry to connect to server
-    if (retryNumber > 0) {
-      setTimeout(() => {
-        connectToMongo(uriConnect, retryNumber - 1)
-      }, 10000)
-    }
+    retryMongoConnection(uriConnect, retryNumber)
     return
+  }
+}
+
+const retryMongoConnection = (uriConnect, retryNumber) => {
+  if (retryNumber > 0) {
+    setTimeout(() => {
+      connectToMongo(uriConnect, retryNumber - 1)
+    }, 10000)
   }
 }
 
