69bef946b0111a6ed93219b7ce14bd18b9d19b70
Ankit Gupta <ankit@original4sure.com>
2018-12-01 18:06:15 +0530

    file name change

--- src/event-components/client-parent-event.ts
+++ src/event-components/client-parent-event.ts
@@ -2,8 +2,8 @@ import { fork } from "child_process"
 import * as fs from "fs"
 import * as logger from "winston"
 
-import { watcherMain } from "./client-events/watcher-clients"
-// fork(`${__dirname}/client-events/watcher-clients`)
+import { watcherMain } from "./client-events/watchers"
+// fork(`${__dirname}/client-events/watcher-clients`) 
 
 
 export const clientParent = () => {
